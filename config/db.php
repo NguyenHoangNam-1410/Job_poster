cec7a523a7e92155c24fd6b60f0be3846512e265
nguyendangcole <nguyenduydang225@gmail.com>
2025-11-07 13:54:03 +0700

    db_password

--- config/db.php
+++ config/db.php
@@ -8,7 +8,7 @@ class Database
     private $servername = "localhost";
     private $username = "root";
     private $db_name = "job_poster";
-    private $db_password = "";
+    private $db_password = "root";
 
     public function __construct()
     {
