29da115589709309663fcfe319fae92d92b6c3ff
Nguyen Hoang Nam <nam.nguyensilv666@hcmut.edu.vn>
2025-11-09 22:51:53 +0700

    resolve conflict

--- app/helpers/Icons.php
+++ app/helpers/Icons.php
@@ -469,8 +469,6 @@ class Icons
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path>
         </svg>';
     }
-}
-?>
 
     /** 🛡️ Shield Check Icon (camelCase alias) */
     public static function shieldCheck($classes = null)
@@ -478,3 +476,4 @@ class Icons
         return self::shield_check($classes);
     }
 }
+?>
