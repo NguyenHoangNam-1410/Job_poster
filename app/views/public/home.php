720da1c0fb9b193d8a27cf6d5b2820a9c5a4a0f1
nguyendangcole <nguyenduydang225@gmail.com>
2025-11-23 02:24:20 +0700

    change

--- app/views/public/home.php
+++ app/views/public/home.php
@@ -525,7 +525,7 @@ include __DIR__ . '/../layouts/public_header.php';
     <!-- Video container -->
     <div class="retro-card max-w-5xl mx-auto bg-white p-6">
       <div class="relative overflow-hidden border-3 border-gray-900" style="padding-bottom: 56.25%; height: 0;">
-        <iframe class="absolute top-0 left-0 w-full h-full" src="https://www.youtube.com/embed/GOqARrckaR8"
+        <iframe class="absolute top-0 left-0 w-full h-full" src="https://www.youtube.com/embed/IawcFovAQRw"
           title="WorkNest Intro Video" frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen>
